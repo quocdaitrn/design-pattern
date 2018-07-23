9d379cbdc54638ea8daa41928d1490de8118ae5b
DaiTran <daitq.cntt@gmail.com>
2018-07-23 09:36:54 +0700

    add code sample for factory method pattern

---
+++ src/main/java/vn/self/training/designpattern/factorymethod/Rectangle.java
@@ -0,0 +1,8 @@
+package vn.self.training.designpattern.factorymethod;
+
+public class Rectangle implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("Draw Rectangle");
+    }
+}
---
+++ src/main/java/vn/self/training/designpattern/factorymethod/Shape.java
@@ -0,0 +1,17 @@
+package vn.self.training.designpattern.factorymethod;
+
+import java.lang.reflect.UndeclaredThrowableException;
+
+public interface Shape {
+    void draw();
+
+    static Shape create(String s) {
+        if (s.startsWith("#rect")) {
+            return new Rectangle();
+        } else if (s.startsWith("#oval")) {
+            return new Oval();
+        }
+
+        throw new UndeclaredThrowableException(new Exception());
+    }
+}
